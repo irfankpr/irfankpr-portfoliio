63cf407ec0796084d0160a6a8eeaf0bb3511e6d1
irfankpr <irfanmuhammedkpr@gmail.com>
2023-07-20 17:18:11 +0530

    preloader set

--- statics/main.js
+++ statics/main.js
@@ -1,3 +1,7 @@
-function main(){
-    
-}
+document.addEventListener("DOMContentLoaded", function() {
+    // When the DOM content is loaded, remove the preloader
+    var preloader = document.getElementById("preloader");
+    setTimeout(function() {
+        preloader.id="";
+      }, 250);
+  })
