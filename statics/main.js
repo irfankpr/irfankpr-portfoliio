46fa593a2131db847716fa5d416209848400575d
irfankpr <irfanmuhammedkpr@gmail.com>
2023-07-22 12:49:24 +0530

    about done

--- statics/main.js
+++ statics/main.js
@@ -1,7 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     // When the DOM content is loaded, remove the preloader
     var preloader = document.getElementById("preloader");
-    setTimeout(function() {
-        preloader.id="";
-      }, 250);
+    preloader.style.display = "none";
+    preloader.id="";
   })
